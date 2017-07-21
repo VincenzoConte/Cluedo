f5c93fd003ac48f943ffd2864b87dc1b5bba24c1
iFreddie <fabry-badboy@hotmail.it>
2017-07-21 17:34:11 +0200

    Dadi

---
+++ Assets/ModelliComuni/dadi/dice.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+ using System.Collections;
+ using System.Collections.Generic;
+ 
+ public class dice : MonoBehaviour {
+ 
+     public List<Vector3> directions;
+     public List<int> sideValues;
+      public float forceAmount = 10.0f;
+      public float torqueAmount = 10.0f;
+      public ForceMode forceMode;
+      Rigidbody rb;
+      bool flag;
+     void Start ()
+	{
+		if (directions.Count == 0) {
+			// Object space directions
+			directions.Add (Vector3.up);
+			sideValues.Add (5); // up
+			directions.Add (Vector3.down);
+			sideValues.Add (2); // down
+ 
+			directions.Add (Vector3.left);
+			sideValues.Add (3); // left
+			directions.Add (Vector3.right);
+			sideValues.Add (4); // right
+ 
+			directions.Add (Vector3.forward);
+			sideValues.Add (1); // fw
+			directions.Add (Vector3.back);
+			sideValues.Add (6); // back
+			rb = this.GetComponent<Rigidbody> ();
+			flag = true;
+		}
+	}
+
+     void Update(){
+     if(Input.GetKey (KeyCode.D)){          //lancia
+     rb.AddForce (Random.onUnitSphere*forceAmount, forceMode);
+     rb.AddTorque (Random.onUnitSphere*torqueAmount, forceMode);
+			flag = false;
+     }
+
+     if(rb.IsSleeping ()){
+			if(flag==false){
+				Debug.Log("Risultato lancio: " + GetNumber(Vector3.up, 30f));
+				flag = true;
+			}
+     }
+     }
+
+
+     public int GetNumber(Vector3 referenceVectorUp, float epsilonDeg = 5f) {
+         Vector3 referenceObjectSpace = transform.InverseTransformDirection(referenceVectorUp);
+ 
+         float min = float.MaxValue;
+         int mostSimilarDirectionIndex = -1;
+         for (int i=0; i < directions.Count; ++i) {
+             float a = Vector3.Angle(referenceObjectSpace, directions[i]);
+             if (a <= epsilonDeg && a < min) {
+                 min = a;
+                 mostSimilarDirectionIndex = i;
+             }
+         }
+         return (mostSimilarDirectionIndex >= 0) ? sideValues[mostSimilarDirectionIndex] : -1; 
+     }
+ }
