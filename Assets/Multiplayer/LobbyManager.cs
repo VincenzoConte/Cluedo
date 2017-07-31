bacc7b650584b10af22b0babf4790f9f3d904f67
Vincenzo Conte <vincenzoconte@hotmail.com>
2017-07-31 12:17:23 +0200

    modifiche

--- Assets/Multiplayer/LobbyManager.cs
+++ Assets/Multiplayer/LobbyManager.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
 
 public class LobbyManager : NetworkLobbyManager {
+
+    protected ulong _currentMatchID;
 
     public override void OnStartHost()
     {
         base.OnStartHost();
         //cambio interfaccia
+    }
+
+    public override void OnMatchCreate(bool success, string extendedInfo, MatchInfo matchInfo)
+    {
+        base.OnMatchCreate(success, extendedInfo, matchInfo);
+        _currentMatchID = (System.UInt64)matchInfo.networkId;
     }
 }
