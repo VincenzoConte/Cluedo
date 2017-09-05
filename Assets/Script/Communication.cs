c1185de77d38764f36b5e08f4cb7ceaaf6724cb9
Vincenzo Conte <vincenzoconte@hotmail.com>
2017-09-05 14:14:09 +0200

    prove

--- Assets/Script/Communication.cs
+++ Assets/Script/Communication.cs
@@ -1,26 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
 
 public class Communication : NetworkBehaviour {
 
     NetworkConnection[] players;
     int turno;
+    public const short msg = 1000;
 
 	// Use this for initialization
 	void Start () {
-        if (isServer)
+        //NetworkManager.singleton.client.RegisterHandler(MsgType.NetworkInfo, InizioTurno);
+        if (NetworkServer.active)
         {
             turno = 0;
             players = new NetworkConnection[NetworkServer.connections.Count];
             NetworkServer.connections.CopyTo(players, 0);
-            TargetCambioTurno(players[turno+1], 0);
+            /*for (int i = 0; i < players.Length; i++)
+            {
+                TargetCambioTurno(players[i], i);
+            }*/
+            /*players[1].RegisterHandler(1000, InizioTurno);
+            players[1].InvokeHandler(1000, new NetworkReader(), Channels.DefaultReliable);
+            NetworkServer.SendToClient(players[1].connectionId, MsgType.Highest+5, new EmptyMessage());*/
+            TargetCambioTurno(players[1], 0);
         }
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    /*private void InizioTurno(NetworkMessage netMsg)
+    {
+        this.gameObject.GetComponent<OperativaInterfaccia>().setTurnoTrue();
+        Debug.Log("mio turno");
+    }*/
+
+    // Update is called once per frame
+    void Update () {
 	}
 
     [TargetRpc]
